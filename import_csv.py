08919bd6d6fdb117ae620172670653363a82cec2
walingsonCostaUnemat <64569036+walingsonCostaUnemat@users.noreply.github.com>
2023-02-18 23:04:11 -0400

    Update import_csv.py

--- import_csv.py
+++ import_csv.py
@@ -1,6 +1,40 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+def main():
+    option = st.sidebar.text_input("I - Digite o separador do seu .CSV",',')
+    
+    uploaded_file = st.sidebar.file_uploader("II - Buscar arquivo CSV ", type="csv")
+#função que carrega um arquivo .CSV e transforma num Dataframe
+    def load_data(uploaded_file,option):
+        try:
+            data = pd.read_csv(uploaded_file,sep=option,thousands = '.', decimal = ',')
+            data['Data'] = pd.to_datetime(data['Data'],format='%Y/%m/%d',errors='ignore')            
+            return data
+        except KeyError: 
+            st.sidebar.error('Selecione o separador correto')
+        except TypeError: 
+            data = load_data(uploaded_file,option)
+        except AttributeError:    
+            st.error('Algo deu errado')
+    
+        
+    if uploaded_file is not None:
+        data = load_data(uploaded_file,option)
+        df = data
+        try:
+            data.to_csv('data.csv',sep=option)
+            #Ver dados brutos        
+            #visualizar dados em branco
+            st.write(df)
 
+            
+        
+  
+    
+
+
+           
+main()  
 
              
